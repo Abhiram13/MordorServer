6a24f3980a6241935841200dc853fe30bd8b9333
Abhiram13 <abhiram131869@gmail.com>
2020-11-23 21:33:26 +0530

    code format change

--- authenticateHeaders.cs
+++ authenticateHeaders.cs
@@ -4,21 +4,27 @@ using MongoDB.Driver;
 using MongoDB.Bson;
 using System.Collections.Generic;
 
-namespace MordorServer {
-   public abstract class String {
-      public virtual string Encode(string str) {
+namespace MordorServer
+{
+   public abstract class String
+   {
+      public virtual string Encode(string str)
+      {
          var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(str);
          return System.Convert.ToBase64String(plainTextBytes);
       }
 
-      public virtual string Decode(string str) {
+      public virtual string Decode(string str)
+      {
          var base64EncodedBytes = System.Convert.FromBase64String(str);
          return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
       }
    }
 
-   public class Auth : Token {
-      public void Headers(HttpListenerRequest request) {
+   public class Auth : Token
+   {
+      public void Headers(HttpListenerRequest request)
+      {
          string[] values = request.Headers.GetValues("Token");
          string tokenHeader = values[0];
          Generate(tokenHeader);
--- token.cs
+++ token.cs
@@ -4,43 +4,67 @@ using MongoDB.Driver;
 using MongoDB.Bson;
 using System.Collections.Generic;
 
-namespace MordorServer {
-   public abstract class Token : String {
-      private string Create(string header) {
+namespace MordorServer
+{
+   public abstract class Token : String
+   {
+
+      private IMongoCollection<IToken> TokenCollection()
+      {
+         return Mongo.database.GetCollection<IToken>("tokens");
+      }
+
+      private IToken[] Tokens()
+      {
+         IToken[] tokens = this.TokenCollection().Find<IToken>(new BsonDocument()).ToList<IToken>().ToArray();
+         return tokens;
+      }
+
+      private string Create(string header)
+      {
          string username = header.Split(":")[0];
          string password = header.Split(":")[1];
          return Encode($"{username}_{password}_{DateTimeOffset.UtcNow.ToLocalTime().ToString()}");
       }
 
-      private async void Kill(string header) {
-         IMongoCollection<IToken> tokenCollection = Mongo.database.GetCollection<IToken>("tokens");
+      private async void Kill(string header)
+      {
          string username = header.Split(":")[0];
          string password = header.Split(":")[1];
-         var filter = Builders<IToken>.Filter.Eq("username", username);
-         var update = Builders<IToken>.Update.Set("Token", "");
 
          await System.Threading.Tasks.Task.Delay(20000);
 
-         tokenCollection.UpdateOne(filter, update);
+         this.TokenCollection().UpdateOne(
+            Builders<IToken>.Filter.Eq("username", username),
+            Builders<IToken>.Update.Set("Token", "")
+         );
       }
 
-      public virtual void Generate(string header) {
-         IMongoCollection<IToken> tokenCollection = Mongo.database.GetCollection<IToken>("tokens");
-         IToken[] tokens = tokenCollection.Find<IToken>(new BsonDocument()).ToList<IToken>().ToArray();
+      public virtual void Generate(string header)
+      {
          string username = header.Split(":")[0];
          string password = header.Split(":")[1];
 
-         IToken token = Array.Find<IToken>(tokens, t => t.username == username);
-         
-         if (token == null) {
-            tokenCollection.InsertOne(new IToken { username = username, password = password, Token = Create(header), _id = ObjectId.GenerateNewId() });
-         } else {
-            var filter = Builders<IToken>.Filter.Eq("username", username);
-            var update = Builders<IToken>.Update.Set("Token", Create(header));
-            UpdateResult x = tokenCollection.UpdateOne(filter, update);
+         IToken token = Array.Find<IToken>(this.Tokens(), t => t.username == username);
+
+         if (token == null)
+         {
+            this.TokenCollection().InsertOne(new IToken { username = username, password = password, Token = Create(header), _id = ObjectId.GenerateNewId() });
+         }
+         else
+         {
+            this.TokenCollection().UpdateOne(
+               Builders<IToken>.Filter.Eq("username", username),
+               Builders<IToken>.Update.Set("Token", Create(header)
+            ));
          }
 
          Kill(header);
+      }
+
+      public virtual void FindToken(string token)
+      {
+         IToken tokn = Array.Find<IToken>(this.Tokens(), t => t.Token == token);
       }
    }
 }
